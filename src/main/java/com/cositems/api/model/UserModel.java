21e5e140a07217c917faf6754590203def48fb9b
João Gabriel Cassuriaga da Cruz <gabriel_cassuriaga@hotmail.com>
2025-08-17 23:02:01 -0300

    fix(model): corrige collection do UserModel para 'users'

--- src/main/java/com/cositems/api/model/UserModel.java
+++ src/main/java/com/cositems/api/model/UserModel.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
-@Document(collection = "products")
+@Document(collection = "users")
 public class UserModel {
     @Id
     private String username;
