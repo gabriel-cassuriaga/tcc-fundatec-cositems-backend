4f8b343f4fc50cb9e62a9d4be2cbb8fc71c206df
João Gabriel Cassuriaga da Cruz <gabriel_cassuriaga@hotmail.com>
2025-08-17 22:47:03 -0300

    feat(model): adiciona UserModel com username, email e password

---
+++ src/main/java/com/cositems/api/model/UserModel.java
@@ -0,0 +1,20 @@
+package com.cositems.api.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Document(collection = "products")
+public class UserModel {
+    @Id
+    private String username;
+    private String email;
+    private String password;
+
+}
