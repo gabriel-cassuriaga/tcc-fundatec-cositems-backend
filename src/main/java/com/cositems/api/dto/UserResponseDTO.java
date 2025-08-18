b0eefd18ada66b7fa054a389f931b99bdc0963a0
João Gabriel Cassuriaga da Cruz <gabriel_cassuriaga@hotmail.com>
2025-08-17 23:35:15 -0300

    fix(dto): adiciona construtor de UserModel em UserResponseDTO

--- src/main/java/com/cositems/api/dto/UserResponseDTO.java
+++ src/main/java/com/cositems/api/dto/UserResponseDTO.java
@@ -1,5 +1,11 @@
 package com.cositems.api.dto;
 
+import com.cositems.api.model.UserModel;
+
 public record UserResponseDTO(String username, String email) {
+
+    public UserResponseDTO(UserModel user) {
+        this(user.getUsername(), user.getEmail());
+    }
     
 }
