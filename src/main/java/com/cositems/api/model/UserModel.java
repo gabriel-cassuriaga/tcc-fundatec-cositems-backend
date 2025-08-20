2b9bbf2d5e263dac62f2c5304e2658b99b3ce1a0
João Gabriel Cassuriaga da Cruz <gabriel_cassuriaga@hotmail.com>
2025-08-19 22:50:08 -0300

    chore(model): adiciona @NoArgsConstructor e @SuperBuilder ao UserModel

--- src/main/java/com/cositems/api/model/UserModel.java
+++ src/main/java/com/cositems/api/model/UserModel.java
@@ -5,11 +5,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
 @Document(collection = "users")
 public class UserModel {
     @Id
