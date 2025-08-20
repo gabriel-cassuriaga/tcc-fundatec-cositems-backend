5e79aa79c46974250c9e0bb8eecfba36acd9621b
João Gabriel Cassuriaga da Cruz <gabriel_cassuriaga@hotmail.com>
2025-08-20 17:48:50 -0300

    feat(dto): incluir campo id no UserResponseDTO

--- src/main/java/com/cositems/api/dto/UserResponseDTO.java
+++ src/main/java/com/cositems/api/dto/UserResponseDTO.java
@@ -2,10 +2,10 @@ package com.cositems.api.dto;
 
 import com.cositems.api.model.UserModel;
 
-public record UserResponseDTO(String username, String email) {
+public record UserResponseDTO(String id, String username, String email) {
 
     public UserResponseDTO(UserModel user) {
-        this(user.getUsername(), user.getEmail());
+        this(user.getId(), user.getUsername(), user.getEmail());
     }
     
 }
