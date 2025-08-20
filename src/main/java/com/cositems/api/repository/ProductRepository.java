cf39fc18c898cce7dfa222cdd44a656332b131d5
João Gabriel Cassuriaga da Cruz <gabriel_cassuriaga@hotmail.com>
2025-08-19 23:31:04 -0300

    Revert "feat(repository): Adiciona ShoppingCartRepository"
    This reverts commit b8a6dd46eaaad362c4c3ab7d7524bdff82dcb186.

--- src/main/java/com/cositems/api/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cositems.api.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.cositems.api.model.ProductModel;
-
-public interface ProductRepository extends MongoRepository<ProductModel, String> {
-
-}
