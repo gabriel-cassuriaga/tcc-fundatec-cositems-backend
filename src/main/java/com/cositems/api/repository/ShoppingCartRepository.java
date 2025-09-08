d45ea009e86696ee021f71482257afe5257120e9
João Gabriel Cassuriaga da Cruz <gabriel_cassuriaga@hotmail.com>
2025-09-07 22:04:04 -0300

    feat(repository): adiciona método findByCustomerId no ShoppingCartRepository

--- src/main/java/com/cositems/api/repository/ShoppingCartRepository.java
+++ src/main/java/com/cositems/api/repository/ShoppingCartRepository.java
@@ -1,9 +1,11 @@
 package com.cositems.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.cositems.api.model.ShoppingCart;
 
 public interface ShoppingCartRepository extends MongoRepository<ShoppingCart, String> {
-
+    Optional<ShoppingCart> findByCustomerId(String customerId);
 }
