40b6638448fc466cddb02782e0bb7ae7a4ba691e
João Gabriel Cassuriaga da Cruz <gabriel_cassuriaga@hotmail.com>
2025-09-10 23:28:46 -0300

    feat(repository): adiciona métodos de busca por username e email no UserRepository

--- src/main/java/com/cositems/api/repository/UserRepository.java
+++ src/main/java/com/cositems/api/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.cositems.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.cositems.api.model.UserModel;
 
 public interface UserRepository extends MongoRepository<UserModel, String> {
+    Optional<UserModel> findByUsername(String username);
+
+    Optional<UserModel> findByEmail(String email);
 
 }
