ed89c16574886e003a1433fae40ac329dd8d0a8d
João Gabriel Cassuriaga da Cruz <gabriel_cassuriaga@hotmail.com>
2025-09-11 00:18:31 -0300

    feat(repository): adiciona métodos de busca por nome, e por sellerId e nome em conjunto no ProductRepository

--- src/main/java/com/cositems/api/repository/ProductRepository.java
+++ src/main/java/com/cositems/api/repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.cositems.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.cositems.api.model.ProductModel;
 
 public interface ProductRepository extends MongoRepository<ProductModel, String> {
+    Optional<ProductModel> findByName(String name);
+    Optional<ProductModel> findBySellerIdAndName(String sellerId, String name);
 
 }
