4950c0966b5aab0919997eaa35434cdb3800c65b
João Gabriel Cassuriaga da Cruz <gabriel_cassuriaga@hotmail.com>
2025-09-21 23:35:56 -0300

    feat(security): implementa UserDetails na entidade UserModel

--- src/main/java/com/cositems/api/model/UserModel.java
+++ src/main/java/com/cositems/api/model/UserModel.java
@@ -1,7 +1,13 @@
 package com.cositems.api.model;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,11 +21,51 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @SuperBuilder
 @Document(collection = "users")
-public class UserModel {
+public class UserModel implements UserDetails {
     @Id
     private String id;
-    private String username;
+    private String displayName;
     private String email;
     private String password;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this instanceof Seller) {
+            return List.of(new SimpleGrantedAuthority("ROLE_SELLER"));
+        } else if (this instanceof Customer) {
+            return List.of(new SimpleGrantedAuthority("ROLE_CUSTOMER"));
+        }
+
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
