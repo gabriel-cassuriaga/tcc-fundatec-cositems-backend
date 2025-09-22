d19039ff76f4e94d50688e7e3da6635e9d6db584
João Gabriel Cassuriaga da Cruz <gabriel_cassuriaga@hotmail.com>
2025-09-22 00:25:27 -0300

    refactor(controller): melhora injeção de dependência e nomenclatura no ShoppingCartController

--- src/main/java/com/cositems/api/controller/ShoppingCartController.java
+++ src/main/java/com/cositems/api/controller/ShoppingCartController.java
@@ -3,40 +3,41 @@ package com.cositems.api.controller;
 import com.cositems.api.dto.CartItemDTO;
 import com.cositems.api.dto.ShoppingCartResponseDTO;
 import com.cositems.api.service.ShoppingCartService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/carts")
-@AllArgsConstructor
+@RequestMapping("/cart")
+@RequiredArgsConstructor
 public class ShoppingCartController {
 
     private final ShoppingCartService cartService;
 
     @GetMapping("/{customerId}")
-    public ResponseEntity<ShoppingCartResponseDTO> getCart(@PathVariable String customerId) {
+    public ResponseEntity<ShoppingCartResponseDTO> getMyCart(@PathVariable String customerId) {
         ShoppingCartResponseDTO cart = cartService.getCartByCustomerId(customerId);
         return new ResponseEntity<>(cart, HttpStatus.OK);
     }
 
     @PostMapping("/{customerId}/items")
-    public ResponseEntity<ShoppingCartResponseDTO> addItemToCart(@PathVariable String customerId,
+    public ResponseEntity<ShoppingCartResponseDTO> addItemToMyCart(@PathVariable String customerId,
             @RequestBody CartItemDTO itemDto) {
         ShoppingCartResponseDTO cart = cartService.addItemToCart(customerId, itemDto);
         return new ResponseEntity<>(cart, HttpStatus.OK);
     }
 
     @DeleteMapping("/{customerId}/items/{productId}")
-    public ResponseEntity<ShoppingCartResponseDTO> removeItemFromCart(@PathVariable String customerId,
+    public ResponseEntity<ShoppingCartResponseDTO> removeItemFromMyCart(@PathVariable String customerId,
             @PathVariable String productId) {
         ShoppingCartResponseDTO cart = cartService.removeItemFromCart(customerId, productId);
         return new ResponseEntity<>(cart, HttpStatus.OK);
     }
 
     @DeleteMapping("/{customerId}/items")
-    public ResponseEntity<ShoppingCartResponseDTO> clearCart(@PathVariable String customerId) {
+    public ResponseEntity<ShoppingCartResponseDTO> clearMyCart(@PathVariable String customerId) {
         ShoppingCartResponseDTO cart = cartService.clearCart(customerId);
         return new ResponseEntity<>(cart, HttpStatus.OK);
     }
