fbd2fd12d98681812a97196a586464ef49357e0c
João Gabriel Cassuriaga da Cruz <gabriel_cassuriaga@hotmail.com>
2025-09-23 00:43:51 -0300

    refactor(dto): remove dados desnecessários do OrderRequestDTO

--- src/main/java/com/cositems/api/dto/OrderRequestDTO.java
+++ src/main/java/com/cositems/api/dto/OrderRequestDTO.java
@@ -1,11 +1,9 @@
 package com.cositems.api.dto;
 
-import java.math.BigDecimal;
 import java.util.List;
 
-public record OrderRequestDTO(String userId, List<OrderItemRequestDTO> items) {
-            
-    public record OrderItemRequestDTO(String productId, String name, int quantity, BigDecimal price) {
+public record OrderRequestDTO(List<OrderItemRequestDTO> items) {
+    public record OrderItemRequestDTO(String productId, int quantity) {
     }
 
 }
