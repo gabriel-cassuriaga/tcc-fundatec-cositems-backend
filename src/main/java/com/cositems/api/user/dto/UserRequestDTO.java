b7332000c26682d6610124aa66310d2ed2256628
João Gabriel Cassuriaga da Cruz <gabriel_cassuriaga@hotmail.com>
2025-09-28 21:35:22 -0300

    feat(user): flexibiliza validação do nome de usuário

--- src/main/java/com/cositems/api/user/dto/UserRequestDTO.java
+++ src/main/java/com/cositems/api/user/dto/UserRequestDTO.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.Size;
 public record UserRequestDTO(
         @NotBlank(message = "O nome de usuário não pode ser vazio.")
         @Size(min = 3, max = 20, message = "O nome de usuário deve ter entre 3 e 20 caracteres.")
-        @Pattern(regexp = "^[a-zA-Z0-9_.-]+$", message = "O nome de usuário contém caracteres inválidos.")
+        @Pattern(regexp = "^[\\p{L}\\p{N}_. -]+$", message = "O nome de usuário contém caracteres inválidos.")
         String displayName,
 
         @NotBlank(message = "O e-mail não pode ser vazio.")
